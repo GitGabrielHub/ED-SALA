aa7add7267a0b61bbdf61ef3fef2df151e827e38
Gabriel <39504004+GitGabrielHub@users.noreply.github.com>
2019-04-25 20:56:47 -0300

    Update and rename fila.c to fila_enc.c

--- fila_enc.c
+++ fila_enc.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "fila.h"
+#include "fila_enc.h"
 
 
 void criarFila(Tfila *fila ){
@@ -56,4 +56,38 @@ void imprimir( Tfila fila ){
 		fila.inicio = (fila.inicio) -> prox;
 	}
    	printf("]\n");
+}
+
+void fila_alternada (Tfila fila_1, Tfila fila_2,Tfila* fila_3){
+
+	int tam_1 = tamanhoFila(fila_1), tam_2 = tamanhoFila(fila_2);
+	no *aux_1 = fila_1.inicio, *aux_2 = fila_2.inicio;
+	
+	while( tam_1 != 0 && tam_2 != 0){
+
+		enfileirar(fila_3, aux_1 -> dado);
+		enfileirar(fila_3, aux_2 -> dado);
+		aux_1 = aux_1 -> prox;
+		aux_2 = aux_2 -> prox;
+
+		tam_1--;
+		tam_2--;
+		
+	}
+
+	if (tam_1 != 0){
+		while( tam_1 != 0){
+			enfileirar(fila_3, aux_1 -> dado);
+			aux_1 = aux_1 -> prox;
+			tam_1--;
+		}
+	}
+
+	else if (tam_2 != 0){
+		while( tam_2 != 0){
+			enfileirar(fila_3, aux_2 -> dado);
+			aux_2 = aux_2 -> prox;
+			tam_2--;
+		}
+	}
 }
