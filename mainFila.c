2c6883a919dc49a93d481e20bd010d98296a2005
Gabriel <39504004+GitGabrielHub@users.noreply.github.com>
2019-04-25 20:57:26 -0300

    Update mainFila.c

--- mainFila.c
+++ mainFila.c
@@ -1,19 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "fila.h"
+#include "fila_enc.h"
 
 int main(void){
 	int valor;
-	Tfila fila;
+	Tfila fila,fila_2,fila_3;
 	criarFila(&fila);
+	criarFila(&fila_2);
+	criarFila(&fila_3);
 
 	if(filaVazia(fila)) printf("Vazia\n");
 
 	enfileirar(&fila,100);
 	enfileirar(&fila,200);
 	enfileirar(&fila,300);
-	enfileirar(&fila,400);
-	enfileirar(&fila,500);
+	
+	enfileirar(&fila_2,400);
+	enfileirar(&fila_2,500);
+	enfileirar(&fila_2,600);
+	enfileirar(&fila_2,700);
+	enfileirar(&fila_2,800);
+
+	fila_alternada (fila,fila_2,&fila_3);
+	
 	desenfileirar(&fila);
 
 	elementoFrente(fila,&valor);
@@ -21,7 +30,7 @@ int main(void){
 
 	printf("Tamanho da fila: %d\n", fila.tamanho);
 
-	imprimir(fila);
+	imprimir(fila_3);
 
 	return 0;
 }
